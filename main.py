9454d4747a4b4449548c4cf9a3e0641343741c90
Curtfoglaw <curtlawrence1@gmail.com>
2026-06-30 23:59:35 +1000

    added some activation functions

--- main.py
+++ main.py
@@ -8,11 +8,21 @@ hidden_bias = np.zeros(5)
 output_layer_weights = np.random.randn(10, 5) * 0.01
 output_bias = np.zeros(10)
 
+def relu(x):
+    return np.maximum(0, x)
+
+def softmax(x):
+    exp_x = np.exp(x - max(x))
+    return exp_x / np.sum(exp_x)
+
 def forward_pass():
 
-    z = hidden_layer_weights.dot(input_layer)
-    z = z + hidden_bias
+    z_h = hidden_layer_weights.dot(input_layer)
+    z_h = z_h + hidden_bias
+    z_h = relu(z_h)
     
-    print(z)
+    z_o = output_layer_weights.dot(z_h)
+    z_o = z_o + output_bias
+    print(z_o)
 
 forward_pass()
