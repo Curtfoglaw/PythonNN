b7b5013d7557bde174c784f091ef354410751440
Curtfoglaw <curtlawrence1@gmail.com>
2026-07-01 17:33:44 +1000

    added a loss function with cross entropy loss will start on back propogation

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import numpy as np
 
-input_layer = np.random.randn(10)
+y_true = np.array([[0, 0, 1, 0, 0, 0, 0, 0, 0, 0]])
+input_layer = np.random.randn(1, 10)
 
-hidden_layer_weights = np.random.randn(5, 10) * 0.01
-hidden_bias = np.zeros(5)
+hidden_layer_weights = np.random.randn(10, 5) * 0.01
+hidden_bias = np.zeros((1, 5))
 
-output_layer_weights = np.random.randn(10, 5) * 0.01
-output_bias = np.zeros(10)
+output_layer_weights = np.random.randn(5, 10) * 0.01
+output_bias = np.zeros((1, 10))
 
 def relu(x):
     return np.maximum(0, x)
 
 def softmax(x):
-    exp_x = np.exp(x - max(x))
+    exp_x = np.exp(x)
     return exp_x / np.sum(exp_x)
 
-def forward_pass():
+def cross_entropy(y_true, y_pred):
+    loss = -np.sum(y_true * np.log(y_pred))
+    return loss
 
-    z_h = hidden_layer_weights.dot(input_layer)
-    z_h = z_h + hidden_bias
-    z_h = relu(z_h)
+def forward_pass(X):
+
+    z_h = X.dot(hidden_layer_weights) + hidden_bias
+    a_h = relu(z_h)
     
-    z_o = output_layer_weights.dot(z_h)
-    z_o = z_o + output_bias
-    print(z_o)
+    z_o = z_h.dot(output_layer_weights) + output_bias
+    a_o = softmax(z_o)
+    
+    return z_h, a_h, z_o, a_o 
 
-forward_pass()
+def back_prop():
+
+z_h, a_h, z_o, a_o = forward_pass(input_layer)
+
+loss = cross_entropy(y_true, a_o)
+
+print(a_o)
+print(loss)
